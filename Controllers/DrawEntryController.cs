7a762aef5f26d571c208ebbbc64d1770d94d0f44
Oli Newsham <scarylooking@users.noreply.github.com>
2021-04-10 18:15:46 +0100

    Crappy draw entry component

---
+++ Controllers/DrawEntryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Cria.Controllers.Models;
+
+namespace Cria.Controllers
+{
+    [ApiController]
+    [Route("api/drawEntry")]
+    public class DrawEntryController : ControllerBase
+    {
+        private readonly ILogger<DrawEntryController> _logger;
+
+        public DrawEntryController(ILogger<DrawEntryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage CreateEntry([FromBody] DrawEntryRequest whatever)
+        {
+            return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+    }
+}
---
+++ Controllers/Models/DrawEntryRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Cria.Controllers.Models
+{
+    public class DrawEntryRequest
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Prize { get; set; }
+
+        public DrawEntryRequest(){}
+    }
+}
